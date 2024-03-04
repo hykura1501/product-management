80d5946876945bd4363cb0cce1f0f52f543feca9
hykura1501 <22120106@student.hcmus.edu.vn>
2024-03-04 21:59:25 +0700

    admin-dashboard-product

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const express = require("express")
 const route = require("./routes/client/index.route");
-
+const adminRoute = require("./routes/admin/index.route")
+const systemConfig = require("./config/system")
 const database = require("./config/database")
 
 database.connect()
-
-
 
 require('dotenv').config();
 
 const app = express()
 const port = process.env.PORT
 
+
+// App Local Variables
+app.locals.prefixAdmin = systemConfig.prefixAdmin
+
+
 route(app)
+adminRoute(app)
 
 app.set('views', './views')
 app.set('view engine', 'pug')
