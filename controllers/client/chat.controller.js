e59a03825b85d150a72cf1b975a981e7a0c943d4
hykura1501 <22120106@student.hcmus.edu.vn>
2024-05-15 18:17:00 +0700

    simple chat

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,9 +1,28 @@
+const Chat = require("../../models/chat.model")
+const User = require("../../models/user.model")
 //[GET] /cart
 module.exports.index = async (req, res) => {
-  _io.on("connection", (socket) => {
-    console.log("a user connected");
+  _io.once("connection", (socket) => {
+    socket.on("CLIENT_SEND_MESSAGE", async (message) => {
+      //Lưu vào DB
+      const user = res.locals.user
+      const chat = new Chat({
+        content: message,
+        user_id: user.id
+      })
+      
+      await chat.save();
+    })
   });
+
+  const chats = await Chat.find({deleted: false});
+  for (const item of chats) {
+    const inforUser = await User.findOne({_id: item.user_id}).select("fullName")
+    item.inforUser = inforUser
+  }
+
   res.render("client/pages/chat/index", {
     pageTitle: "Chat",
+    chats: chats
   });
 };
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,10 @@
-const socket = io()
+//CLIENT_SEND_MESSAGE
+const formInputMessage = document.querySelector(".form-input-message");
+if (formInputMessage) {
+  formInputMessage.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const message = e.target.elements.message.value
+    socket.emit("CLIENT_SEND_MESSAGE", message)
+    e.target.elements.message.value = "";
+  });
+}
